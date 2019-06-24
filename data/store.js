6115f12b6af5183f026cb3620569c42f017ec6ee
Alpha <versari@pm.me>
2019-06-24 12:13:51 +0200

    Fixed redux dev tools config

--- data/store.js
+++ data/store.js
@@ -1,8 +1,18 @@
-import {createStore, applyMiddleware} from 'redux';
+import {compose, createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
-const makeStore = () => createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+  	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+  		// Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+  	}) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+);
+const makeStore = () => createStore(rootReducer, enhancer);
 
 export default makeStore;
