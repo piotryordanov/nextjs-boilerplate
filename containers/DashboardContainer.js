274c1ea02938945d5c727c1163dbb82264bd0d45
Alpha <versari@pm.me>
2019-06-24 12:06:17 +0200

    Fixed bug in reduc import

--- containers/DashboardContainer.js
+++ containers/DashboardContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {useDispatch, useSelector} from 'redux';
+import {useDispatch, useSelector} from 'react-redux';
 import Dashboard from '../components/Dashboard';
 
 const Index = props => {
